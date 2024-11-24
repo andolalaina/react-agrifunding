3153a8653ca9215b4be2417565fd939da862541a
Harry Michael <ando.lalaina1103@gmail.com>
2024-11-24 08:25:02 +0300

    fix sidebar

--- src/infrastructure/components/sidebar/sidebar.tsx
+++ src/infrastructure/components/sidebar/sidebar.tsx
@@ -1,25 +1,27 @@
 import { Box, ListItemIcon, ListItemText, MenuItem, MenuList, Paper, Typography } from "@mui/material"
 import { Link } from 'react-router-dom'
+import AccountCircleIcon from '@mui/icons-material/AccountCircle'
 import { navigations } from '../../../domain/const/navigations';
+import "./sidebar.css"
 
 export const Sidebar = () => {
   return (
-    <Paper sx={{height: "100vh", padding: ".5rem 1rem", boxSizing: "border-box"}} >
+    <Paper sx={{height: "100vh", padding: ".5rem .25rem", boxSizing: "border-box"}} >
       <Box sx={{display: "flex", flexDirection: "column", justifyContent: "space-between", height: "100%" }}>
         <Box sx={{"display": "flex", "flexFlow": "column nowrap", "justifyContent": "space-around"}}>
           <Box sx={{"display": "flex", "flexFlow": "row nowrap", "justifyContent": "flex-start", "alignItems": "center", "marginBottom": "3rem"}}>
-            <img src="/src/assets/logo.svg" style={{ width: "50px", height: "50px", backgroundColor: "#F0F0F0", borderRadius: "50%" }}/>
-            <Typography>AgriFunding</Typography>
+            <img src="/src/assets/logo.svg" style={{ width: "50px", height: "50px", backgroundColor: "#FFF", borderRadius: "50%" }}/>
+            <Typography className="sidebar-nav-section" variant="h6">AgriFunding</Typography>
           </Box>
           <Box sx={{"display": "flex", "flexFlow": "column nowrap"}}>
-            <Typography variant="overline">Menu principal</Typography>
+            <Typography className="sidebar-nav-section" variant="overline">Menu principal</Typography>
             <MenuList>
             {
               navigations.map((navigationItem) => (
                 <Link key={navigationItem.id} to={navigationItem.url}>
-                  <MenuItem>
-                    <ListItemIcon>{navigationItem.icon()}</ListItemIcon>
-                    <ListItemText>{navigationItem.title}</ListItemText>
+                  <MenuItem className="sidebar-nav-item">
+                    <ListItemIcon className="sidebar-nav-icon">{navigationItem.icon()}</ListItemIcon>
+                    <ListItemText className="sidebar-nav-text">{navigationItem.title}</ListItemText>
                   </MenuItem>
                 </Link>
               ))
@@ -27,11 +29,12 @@ export const Sidebar = () => {
             </MenuList>
           </Box>
         </Box>
-        <Box sx={{"display": "flex", "flexFlow": "row nowrap", "justifyContent": "space-between"}}>
-          <p>logo</p>
-          <p>username</p>
-          <p>actions</p>
-        </Box>
+        <Link to="my-profile">
+            <MenuItem>
+            <ListItemIcon className="sidebar-nav-icon"><AccountCircleIcon /></ListItemIcon>
+            <ListItemText className="sidebar-nav-text">Mon Profil</ListItemText>
+            </MenuItem>
+        </Link>
       </Box>
     </Paper>
   )
--- src/infrastructure/screen/home/app.screen.tsx
+++ src/infrastructure/screen/home/app.screen.tsx
@@ -1,12 +1,8 @@
 import Box from '@mui/material/Box';
 
 import { Outlet } from 'react-router-dom';
-import { Sidebar } from '../../components/sidebar/sidebar';
 import { Grid2 as Grid } from '@mui/material';
-import { MobileSidebar } from '../../components/sidebar/mobileSidebar';
-
-
-
+import { Sidebar } from '../../components/sidebar/sidebar';
 
 
 export const AppScreen = () => {
@@ -14,10 +10,10 @@ export const AppScreen = () => {
   return (
     <Box>
       <Grid container spacing={2}>
-        <Grid size={{"xs": 2, "sm": 3}}>
-          <MobileSidebar />
+        <Grid>
+          <Sidebar />
         </Grid>
-        <Grid component="main" size={{ xs: 10, sm: 9 }} sx={{ padding: "1rem" }}>
+        <Grid component="main" size="grow" sx={{ padding: "1rem" }}>
           <Outlet />
         </Grid>
       </Grid>
