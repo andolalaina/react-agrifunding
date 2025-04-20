734322289d600bf4721b63068ad3b997f87306c7
Michael R. <m.randriajaoson@etechconsulting-mg.com>
2025-04-20 10:20:47 +0300

    fix: change basemap to stadia satellite

--- src/infrastructure/components/Map/index.tsx
+++ src/infrastructure/components/Map/index.tsx
@@ -53,8 +53,10 @@ export const Map = ({center, zoom, markerPosition, markerPopup, customLayers, he
       
       {/* Base OSM layer */}
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        url='https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.jpg'
+        minZoom= {0}
+        maxZoom= {20}
+        // attribution= '&copy; CNES, Distribution Airbus DS, © Airbus DS, © PlanetObserver (Contains Copernicus Data) | &copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
       
       {markerPosition && (
